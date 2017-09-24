a833d14666b495fca57b40b8f7d98e2b6642a711
Parv Kumar <parvathikumar92@gmail.com>
2017-09-24 17:53:37 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -32,7 +32,7 @@ class SongsController < ApplicationController
   end
 
   get '/songs/:slug/edit' do
-    @song = Song.find_by_slug(:slug)
+    @song = Song.find_by_slug(:slug).first
     binding.pry
     erb :"/songs/edit"
   end
